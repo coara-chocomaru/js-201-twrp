a7bda7392a2eba4414206160686051ca9e2693a9
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-09-25 04:01:01 +0900

    Add Android.mk for js201 device configuration

---
+++ Android.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),js201)
+JAVA_SDK_ENFORCEMENT_ERROR := false
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
