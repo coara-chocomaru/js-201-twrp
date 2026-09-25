a7ad83ff0e21c3790bf09c6e514f35517bbb2f5a
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-09-25 14:58:48 +0900

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,7 +118,7 @@ endif
 
 
 # Recovery
-TW_NO_SCREEN_TIMEOUT := true
+# TW_NO_SCREEN_TIMEOUT := true
 TW_NO_SCREEN_BLANK := true
 TARGET_USERIMAGES_USE_EXT4 := true
 RECOVERY_SDCARD_ON_DATA := true
@@ -130,6 +130,7 @@ BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_FSTAB_VERSION := 2
 
 SELINUX_IGNORE_NEVERALLOWS := true
+TW_BRIGHTNESS_PATH := /sys/devices/soc.0/1a00000.qcom,mdss_mdp/qcom,mdss_fb_primary.122/leds/backlightinfo/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 128 
 TW_USE_TOOLBOX := true
